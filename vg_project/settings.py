9f6408b65832721b17188b213505ae2bf1ef8f37
Sparkip <87161541+Sparkip@users.noreply.github.com>
2022-08-22 17:27:03 -0400

    Update settings.py

--- vg_project/settings.py
+++ vg_project/settings.py
@@ -143,3 +143,12 @@ LOGIN_REDIRECT_URL = 'game_home'
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/4.0/howto/static-files/
+
+# The absolute path to the directory where collectstatic will collect static files for deployment.
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+# The URL to use when referring to static files (where they will be served from)
+STATIC_URL = '/static/'
