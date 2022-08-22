4f110c9c16d9c103a90063f9d16a6510e1badaa1
Sparkip Kip <87161541+Sparkip@users.noreply.github.com>
2022-08-22 18:45:53 -0400

    Fixing for heroku

--- vg_project/settings.py
+++ vg_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-^ey+!_(8y3%@+!^x7%xe6h$zt0jyldcw7e!m-u8h%1zx$h6n71
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*',]
 
 
 # Application definition
