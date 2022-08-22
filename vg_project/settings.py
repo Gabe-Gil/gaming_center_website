ceb70ed7003d63656220c9b6d1b333c9ded72f56
Sparkip <87161541+Sparkip@users.noreply.github.com>
2022-08-22 17:38:21 -0400

    Update settings.py

--- vg_project/settings.py
+++ vg_project/settings.py
@@ -51,6 +51,7 @@ REST_FRAMEWORK = {
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
