046493160611e6a0ac211bb84fb80f4724cf5f7e
Sparkip Kip <87161541+Sparkip@users.noreply.github.com>
2022-08-23 12:09:19 -0400

    Fixing for heroku

--- vg_project/settings.py
+++ vg_project/settings.py
@@ -134,6 +134,7 @@ USE_TZ = True
 import os
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),] # new
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
