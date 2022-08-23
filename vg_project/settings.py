6c96ca3ca3cf9de6d39b3b54013f1d44841faa94
Sparkip Kip <87161541+Sparkip@users.noreply.github.com>
2022-08-23 15:33:05 -0400

    Fixing for heroku

--- vg_project/settings.py
+++ vg_project/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^ey+!_(8y3%@+!^x7%xe6h$zt0jyldcw7e!m-u8h%1zx$h6n71'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # settings.py
 LOGGING = {
