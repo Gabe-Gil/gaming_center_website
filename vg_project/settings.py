71c80a1c17fba753f79d846812fdd18a15d64aa4
Sparkip Kip <87161541+Sparkip@users.noreply.github.com>
2022-08-23 17:01:08 -0400

    Fixing for heroku

--- vg_project/settings.py
+++ vg_project/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^ey+!_(8y3%@+!^x7%xe6h$zt0jyldcw7e!m-u8h%1zx$h6n71'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1:8000', 'django-gaming-center.herokuapp.com', '.herokuapp.com/']
 
